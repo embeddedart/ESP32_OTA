a0301bdbeb8e82de44eb45a140f154a11037a1bf
brijesh <brijesh@embeddedart.in>
2024-07-14 22:39:25 +0530

    2024 07 14 2239

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from WIFI_CONFIG import SSID, PASSWORD
 import machine
 import time
 
-firmware_url = "https://github.com/kevinmcaleer/ota_test/main/"
+# firmware_url = "https://github.com/kevinmcaleer/ota_test/main/"
+firmware_url = "https://raw.githubusercontent.com/embeddedart/ESP32_OTA/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test_ota.py")
 
 ota_updater.download_and_install_update_if_available()
 
+time.sleep(3)
 
-
-
-led = machine.Pin(15, machine.Pin.OUT) #configure GPIO-15 Pin as an output pin and create and led object for Pin class
+led = machine.Pin(25, machine.Pin.OUT) #configure GPIO-15 Pin as an output pin and create and led object for Pin class
 
 while True:
-  led.value(True)  #turn on the LED
-  time.sleep(1)   #wait for one second
-  led.value(False)  #turn off the LED
-  time.sleep(1)   #wait for one second
+  # led.value(True)  #turn on the LED
+  # time.sleep(1)   #wait for one second
+  # led.value(False)  #turn off the LED
+  # time.sleep(1)   #wait for one second
+  led.toggle()
+  time.sleep(1)
+  print("toggle")
+
